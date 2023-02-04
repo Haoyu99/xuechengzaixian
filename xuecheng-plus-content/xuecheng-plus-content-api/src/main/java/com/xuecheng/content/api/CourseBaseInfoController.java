dcb3edd97282563342e6fc6cbd9bbead2316c22c
ZHY <852181705@qq.com>
2023-02-04 13:47:01 +0800

    创建PO 创建DTO 创建课程信息编辑接口

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -0,0 +1,38 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.model.dto.QueryCourseParamsDto;
+import com.xuecheng.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * @title CourseBaseInfoController
+ * @description 课程信息编辑接口
+ * @author haoyu99
+ * @version 1.0.0
+ * @creat 2023/2/3 17:20
+ **/
+@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
+@RestController
+public class CourseBaseInfoController {
+
+    /**
+     *
+     * @author haoyu99
+     * @date 2023/2/3 17:27
+     * @param pageParams 分页查询通用参数 以key-value 形式传递
+     * @param queryCourseParams 课程查询参数Dto 以JSon格式 传输 所以要加RequestBody注解
+     * @return PageResult<CourseBase> 返回对象 用于前端解析显示
+     */
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody
+    QueryCourseParamsDto queryCourseParams){
+        return null;
+    }
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/model/po/CourseMarket.java
@@ -0,0 +1,64 @@
+package com.xuecheng.model.po;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+* <p>
+    * 课程营销信息
+    * </p>
+*
+* @author itcast
+*/
+    @Data
+        @EqualsAndHashCode(callSuper = false)
+    @Accessors(chain = true)
+    public class CourseMarket implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+            /**
+            * 主键，课程id
+            */
+    private Long id;
+
+            /**
+            * 收费规则，对应数据字典
+            */
+    private String charge;
+
+            /**
+            * 现价
+            */
+    private Float price;
+
+            /**
+            * 原价
+            */
+    private Float originalPrice;
+
+            /**
+            * 咨询qq
+            */
+    private String qq;
+
+            /**
+            * 微信
+            */
+    private String wechat;
+
+            /**
+            * 电话
+            */
+    private String phone;
+
+            /**
+            * 有效期天数
+            */
+    private Integer validDays;
+
+
+}
