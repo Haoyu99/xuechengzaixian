a85950aae78b8606da13731f459c96879199be4b
ZHY <852181705@qq.com>
2023-02-08 15:31:59 +0800

    配置网关路由以及创建媒资管理的模块

--- xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -3,11 +3,16 @@ package com.xuecheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * @title GatewayApplication
+ * @description
+ * @author haoyu99
+ * @version 1.0.0
+ * @creat 2023/2/8 15:17
+ **/
 @SpringBootApplication
-public class XuechengPlusGatewayApplication {
-
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(XuechengPlusGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class,args);
     }
-
 }
